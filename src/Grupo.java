0ed6143ef07a8fa333435b26e60f3c03f65d4431
fabricioesmerio <fabricio1esmerio@gmail.com>
2022-08-30 22:10:10 -0300

    Criação de app

---
+++ src/Grupo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author fabricio
+ */
+public class Grupo {
+    private int nr;
+    private String nomes;
+
+    public int getNr() {
+        return nr;
+    }
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+
+    public String getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String nomes) {
+        this.nomes = nomes;
+    }
+}
